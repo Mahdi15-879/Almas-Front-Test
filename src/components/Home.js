ab992b442b2869083f6897d92ee6e3e26e89bc84
Mahdi-Heydari <mahdiheydari15879@gmail.com>
2022-07-18 15:17:13 +0430

    conect box to marks

--- src/components/Home.js
+++ src/components/Home.js
@@ -3,7 +3,7 @@ import { MapContainer, TileLayer, Marker } from "react-leaflet";
 import L from "leaflet";
 
 // Components
-import Box from "./Box";
+// import Box from "./Box";
 
 // Styles
 import "./Home.css";
@@ -11,6 +11,11 @@ import "./Home.css";
 // SVG
 import Duotone_1 from "../assets/Duotone-1.svg";
 import Duotone_2 from "../assets/Duotone-2.svg";
+import Diamond from "../assets/Frame.svg";
+import Location_1 from "../assets/Locationn.svg";
+import Calendar from "../assets/Calendar.svg";
+import Left from "../assets/chevron-left.svg";
+import Right from "../assets/chevron-right.svg";
 
 // Locations
 import { locsData } from "../data/locsData";
@@ -18,22 +23,23 @@ import { locsData } from "../data/locsData";
 const position = [32.654629, 51.667984];
 
 const Home = () => {
-  const [isChoose, setIsChoose] = useState(false);
+  const [id, setId] = useState();
 
-  const iconHandler = () => {
-    if (isChoose) {
-      setIsChoose(false);
-      return Duotone_1;
-    } else {
-      return Duotone_2;
-    }
-  };
-
-  let customIcon = L.icon({
-    iconUrl: iconHandler(),
-    iconSize: [35, 50],
+  let customIcon_1 = L.icon({
+    iconUrl: Duotone_2,
+    iconSize: [30, 45],
     iconAnchor: [12.5, 41],
   });
+
+  let customIcon_2 = L.icon({
+    iconUrl: Duotone_1,
+    iconSize: [45, 60],
+    iconAnchor: [12.5, 41],
+  });
+
+  const clickHandler = (id) => {
+    setId(id);
+  };
 
   return (
     <div className="Home">
@@ -47,10 +53,58 @@ const Home = () => {
           <Marker
             key={loc.properties.PARK_ID}
             position={loc.geometry.coordinates}
-            icon={customIcon}
+            icon={id == loc.properties.PARK_ID ? customIcon_2 : customIcon_1}
           />
         ))}
       </MapContainer>
+
+      <img src={Left} alt="Chevron Left" className="arrow-left" />
+      <img src={Right} alt="Chevron Right" className="arrow-right" />
+
+      <div className="Box-container">
+        {locsData.features.map((loc) => (
+          <div
+            key={loc.properties.PARK_ID}
+            className="Box"
+            onClick={() => clickHandler(loc.properties.PARK_ID)}
+          >
+            <div className="Box__row-1">
+              <span>
+                {loc.properties.POINT}
+                <figure>
+                  <img src={Diamond} alt="Diamond" />
+                </figure>
+              </span>
+
+              <h4>{loc.properties.NAME}</h4>
+            </div>
+
+            <div className="Box__row-2">
+              <span>
+                {loc.properties.ADDRESS}
+                <figure>
+                  <img src={Location_1} alt="Location" />
+                </figure>
+              </span>
+
+              <span>
+                {loc.properties.DATE}
+                <figure>
+                  <img src={Calendar} alt="Calendar" />
+                </figure>
+              </span>
+            </div>
+
+            <div className="Box__row-3">
+              <span>کارفرما: {loc.properties.EMPLOYER}</span>
+
+              <h4>
+                <span>باز</span> . 32 پیشنهاد
+              </h4>
+            </div>
+          </div>
+        ))}
+      </div>
     </div>
   );
 };
